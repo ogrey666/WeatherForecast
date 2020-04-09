e10597596073d4acd4f2f9a2bf1080bdba3be249
ogrey666 <kampatg@gmail.com>
2020-04-09 15:40:09 +0200

    Refactor of client response message

--- SerwisKlienckiPogodynkowy/src/main/java/com/example/kurs/SerwisKlienckiPogodynkowy/service/WeatherForecastService.java
+++ SerwisKlienckiPogodynkowy/src/main/java/com/example/kurs/SerwisKlienckiPogodynkowy/service/WeatherForecastService.java
@@ -23,12 +23,16 @@ public class WeatherForecastService {
         this.weatherGateway = weatherGateway;
     }
 
-    public String getForecast(Integer regionId, Integer aura) {
-        WeatherDTO weatherDTO = weatherGateway.getForecast(regionId, aura);
+    private String buildClientResponse(WeatherDTO weatherDTO) {
         return "Powietrze w regionie " + weatherDTO.getRegion()
                 + " jest " + weatherDTO.getDescription()
                 + " z wskaźnikiem PM 2.5 na poziomie " + weatherDTO.getAura()
                 + ", czyli dość " + (weatherDTO.getAura() < 50 ? "niskim" : "wysokim");
+    }
+
+    public String getForecast(Integer regionId, Integer aura) {
+        WeatherDTO weatherDTO = weatherGateway.getForecast(regionId, aura);
+        return buildClientResponse(weatherDTO);
     }
 
     public List<String> getSavedForecasts(Integer regionId, Integer aura) {
@@ -36,21 +40,13 @@ public class WeatherForecastService {
         List<String> stringList = new ArrayList<String>();
         for (WeatherDTO weatherDTO : weatherResponse
              ) {
-            stringList.add(
-                "Powietrze w regionie " + weatherDTO.getRegion()
-                        + " jest " + weatherDTO.getDescription()
-                        + " z wskaźnikiem PM 2.5 na poziomie " + weatherDTO.getAura()
-                        + ", czyli dość " + (weatherDTO.getAura() < 50 ? "niskim" : "wysokim")
-            );
+            stringList.add(buildClientResponse(weatherDTO));
         }
         return stringList;
     }
 
     public String updateForecast(Long forecastId, WeatherDTO weatherDTO) {
         WeatherDTO weatherDTO1 = weatherGateway.updateForecast(forecastId, weatherDTO);
-        return "Powietrze w regionie " + weatherDTO1.getRegion()
-                + " jest " + weatherDTO1.getDescription()
-                + " z wskaźnikiem PM 2.5 na poziomie " + weatherDTO1.getAura()
-                + ", czyli dość " + (weatherDTO1.getAura() < 50 ? "niskim" : "wysokim");
+        return buildClientResponse(weatherDTO1);
     }
 }
