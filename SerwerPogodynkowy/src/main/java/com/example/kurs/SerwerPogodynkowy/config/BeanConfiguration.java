ea1bbca2ff251fc0efbe4a5f704f91f57a633d56
ogrey666 <kampatg@gmail.com>
2020-04-15 20:01:27 +0200

    Przykład użycia CommandLineRunnera do generowania danych dla wstrzykniętego serwisu

--- SerwerPogodynkowy/src/main/java/com/example/kurs/SerwerPogodynkowy/config/BeanConfiguration.java
+++ SerwerPogodynkowy/src/main/java/com/example/kurs/SerwerPogodynkowy/config/BeanConfiguration.java
@@ -4,10 +4,12 @@ import com.example.kurs.SerwerPogodynkowy.repository.ForecastRepository;
 import com.example.kurs.SerwerPogodynkowy.service.ForecastService;
 import com.example.kurs.SerwerPogodynkowy.service.Forecaster;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.ui.ModelMap;
+import sun.tools.jar.CommandLine;
 
 //@PropertySource(value = "application.properties", encoding="UTF-8") //TODO nie działają polskie znaki w konfiguracji
 @Configuration
@@ -20,5 +22,19 @@ public class BeanConfiguration {
     public ForecastService forecastService(ForecastRepository forecastRepository) {
         return new Forecaster(region, forecastRepository);
     }
+
+//    // Bean do generowanie inputa na starcie
+//    @Bean
+//    public CommandLineRunner generuj(ForecastService forecastService) {
+//        return args -> {
+//            forecastService.getForecast();
+//            forecastService.getForecast();
+//            forecastService.getForecast();
+//            forecastService.getForecast();
+//            forecastService.getForecast();
+//            forecastService.getForecast();
+//        };
+//
+//    }
 
 }
